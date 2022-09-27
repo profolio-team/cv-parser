fcf418b1ae419dba1d4bf8e2201e72b4438cfd35
Alex Dmowski <alex.dmowskii@fluxon.com>
2022-09-27 03:11:05 +0200

    Generate form in page - for ability to not close pop-up after focusout from popup

--- src/popup/app.tsx
+++ src/popup/app.tsx
@@ -1,31 +1,12 @@
 import { callFunctionInPage } from '@/utils/callFunctionInPage';
 
+import { injectableMainFunction } from './pagePopup';
+
 export const App = () => {
-  const color = 'red';
+  callFunctionInPage(injectableMainFunction);
+  setTimeout(() => {
+    window.close();
+  }, 10);
 
-  const changeColor = () => {
-    document.body.style.background = 'red';
-  };
-
-  return (
-    <div style={{ display: 'grid', gap: '25px', width: '500px', height: '300px' }}>
-      <form action="/" method="post" style={{ display: 'grid', gap: '10px', width: '100%', height: 'max-content' }}>
-        <label htmlFor="cv-file">Upload cv for parsing</label>
-        <input type="file" id="cv-file" />
-        <button type="submit" style={{ padding: '10px 20px' }}>
-          Parse
-        </button>
-        <div
-          onClick={() => callFunctionInPage(changeColor)}
-          style={{
-            background: color,
-            padding: '5px 10px',
-            color: 'white'
-          }}
-        >
-          change background color
-        </div>
-      </form>
-    </div>
-  );
+  return <></>;
 };
